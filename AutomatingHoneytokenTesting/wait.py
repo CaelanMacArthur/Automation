cc87bf2466b5b516bd8141082697cbb01e67439c
Caelan MacArthur <Caelan_MacArthur@outlook.com>
2021-09-19 13:05:35 -0700

    Update wait.py

--- AutomatingHoneytokenTesting/wait.py
+++ AutomatingHoneytokenTesting/wait.py
@@ -7,17 +7,17 @@ def runWait():
 
 def runWaitUser():
     print("Waiting for user so they can see the screen")
-    time.sleep(10)  # wait for amazon mail server
+    time.sleep(10)  # wait for user to see
     print("Done waiting!")
 
 def runWaitBrowser():
     print("Waiting for page to load.")
-    time.sleep(4)  # wait for amazon mail server
+    time.sleep(4)  # wait for browser
     print("Page should be loaded.")
 
 def runWaitAlexa():
     print("Waiting for alexa.")
-    time.sleep(10)  # wait for amazon mail server
+    time.sleep(10)  # wait for alexa to speak
 
 if __name__ == '__runWait__': 
     runWait()
